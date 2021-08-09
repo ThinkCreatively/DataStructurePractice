31e303f2e88ff8faa069666738eca95fcd75d1ab
James Moore <jameswmoore99@gmail.com>
2021-08-09 12:00:07 -0600

    Implemented file structure for all data structures ~ Finished Linked List

---
+++ dataStructures/linkedList/linkedList.js
@@ -0,0 +1,26 @@
+var LinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+
+  };
+
+  list.removeHead = function() {
+  };
+
+  list.contains = function(target) {
+  };
+
+  return list;
+};
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
