b76960281b71c176211ebfe045f77217f55a7584
James Moore <jameswmoore99@gmail.com>
2021-09-02 17:09:34 -0600

    Added Test Case

--- searchAlgos/BubbleSort/bubbleSort.js
+++ searchAlgos/BubbleSort/bubbleSort.js
@@ -32,4 +32,5 @@ function bubbleSort(array) {
   return array;
 }
 
-console.log(bubbleSort([7, 4, 6, 1, 9, 0])); //[0, 1, 3, 6, 7, 9]
+console.log(bubbleSort([7, 4, 6, 1, 9, 0])); //[0, 1, 4, 6, 7, 9]
+console.log(bubbleSort([7, 4, 6, 1, 9, 0, 8, 11, 3])); // [0, 1, 3, 4, 6, 7, 8, 9, 11]
