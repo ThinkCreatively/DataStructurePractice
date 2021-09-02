9c582ea65b06d9e543aaae2b9a1bbfc72fd2a3a4
James Moore <jameswmoore99@gmail.com>
2021-09-02 17:18:04 -0600

    Updated bubbleSort

--- searchAlgos/BubbleSort/bubbleSort.js
+++ searchAlgos/BubbleSort/bubbleSort.js
@@ -22,7 +22,7 @@
  END: [0, 1, 3, 6, 7, 9]
  */
 function bubbleSort(array) {
-  for (let i = array.length - 1; i >= 0; i--) {
+  for (let i = array.length; i > 0; i--) {
     for (let j = 0; j <= i - 1; j++) {
       if (array[j] > array[j + 1]) {
         [array[j], array[j+1]] = [array[j + 1], array[j]]
