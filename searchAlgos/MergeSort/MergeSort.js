6fb913d16c0b8b27a24d6f323cc8139a8ebbda64
James Moore <jameswmoore99@gmail.com>
2021-09-09 16:49:12 -0600

    Implemented a secpmd merge function that uses pointers

--- searchAlgos/MergeSort/MergeSort.js
+++ searchAlgos/MergeSort/MergeSort.js
@@ -18,4 +18,22 @@ function merge(array, array2) {
   return result;
 }
 
+
+function merge(array, array2) {
+  let result = [];
+  let pointerOne = 0;
+  let pointerTwo = 0;
+  let length = array.length + array2.length;
+  while (result.length !== length) {
+    if (array[pointerOne] < array2[pointerTwo]) {
+      result.push(array[pointerOne]);
+      pointerOne++;
+    } else {
+      result.push(array2[pointerTwo]);
+      pointerTwo++;
+    }
+  }
+  return result;
+}
 console.log(merge([1, 10, 50], [2, 14, 99, 100])); // [1, 2, 10, 14, 50, 99, 100]
+console.log(merge([1, 11, 54, 98], [2, 14, 99, 100])); // [1, 2, 11, 14, 54, 98, 99, 100]
