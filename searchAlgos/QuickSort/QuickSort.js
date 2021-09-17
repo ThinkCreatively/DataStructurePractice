bbd3beccc041ce7ff4b2268c0600634430e73f76
James Moore <jameswmoore99@gmail.com>
2021-09-17 14:20:18 -0600

    Implemented quickSort

--- searchAlgos/QuickSort/QuickSort.js
+++ searchAlgos/QuickSort/QuickSort.js
@@ -12,7 +12,8 @@
 
 // Pivot/Partition Helper Function
 // Returns pivot point, Alters input array
-// 1st Attempt
+
+// 1st Attempt (adjusts size of input array)
 // function pivot(array) {
 //   const pivotValue = array[0];
 //   let count = 0;
@@ -27,19 +28,49 @@
 //   }
 //   return pivotValue;
 // }
-// 2nd Attempt
+
+// 2nd Attempt (Swaps values of input array)
 
 function pivot(array, start = 0, end = array.length + 1) {
   let pivot = array[start];
   let swapIndex = start;
-  for (let i = start + 1; i < array.length; i++) {
+  for (let i = start + 1; i <= array.length; i++) {
     if (pivot > array[i]) {
       swapIndex++;
       [array[i], array[swapIndex]] = [array[swapIndex], array[i]];
     }
   }
   [array[start], array[swapIndex]] = [array[swapIndex], array[start]];
-  return pivot;
+  return swapIndex;
 }
 
-console.log(pivot([5, 2, 1, 8, 4, 7, 6, 3])); // [2, 1, 4, 3, 5, 8, 7, 6] returns 5
+// function pivot(arr, start = 0, end = arr.length - 1) {
+//   const swap = (arr, idx1, idx2) => {
+//     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//   };
+//   let pivot = arr[start];
+//   let swapIdx = start;
+//   for (let i = start + 1; i <= end; i++) {
+//     if (pivot > arr[i]) {
+//       swapIdx++;
+//       swap(arr, swapIdx, i);
+//     }
+//   }
+//   swap(arr, start, swapIdx);
+//   return swapIdx;
+// }
+
+// console.log(pivot([5, 2, 1, 8, 4, 7, 6, 3])); // [2, 1, 4, 3, 5, 8, 7, 6] returns 5
+
+// Quick Sort Funciton
+
+function quickSort(array, left = 0, right = array.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(array, left, right);
+    quickSort(array, left, pivotIndex - 1);
+    quickSort(array, pivotIndex + 1, right);
+  }
+  return array;
+}
+
+console.log(quickSort([5, 2, 1, 8, 4, 7, 6, 3])); // [1, 2, 3, 4, 5, 6, 7, 8]
